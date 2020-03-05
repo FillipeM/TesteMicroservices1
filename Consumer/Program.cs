75efe5422f5664f022ec0a4977ad271e3ba6205e
FillipeM <fillipemartins.alves@gmail.com>
2020-03-05 08:18:11 -0300

    .

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topic = "IDGTestTopic";
+            Uri uri = new Uri("http://localhost:9093");
+            var options = new KafkaOptions(uri);
+            var broker = new BrokerRouter(options);
+            var cliente = new KafkaNet.Consumer(new ConsumerOptions(topic, broker));
+
+            List<ObjetoTeste> list = new List<ObjetoTeste>();
+
+            foreach(var message in cliente.Consume())
+            {
+                ObjetoTeste obj = new ObjetoTeste();
+                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+                obj = JsonConvert.DeserializeObject<ObjetoTeste>(Encoding.UTF8.GetString(message.Value));
+                list.Add(obj);
+            }
+
+            Console.WriteLine(list.Count.ToString());
+            Console.ReadLine();
+        }
+    }
+
+    class ObjetoTeste
+    {
+
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string payLoad = "Welcome to Kafka";
+            ObjetoTeste obj = new ObjetoTeste();
+            obj.Codigo = 1;
+            obj.Descricao = "Teste";
+            string topic = "IDGTestTopic";
+            Message msg = new Message(Serialize(obj));
+            Uri uri = new Uri("http://localhost:9093");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var client = new KafkaNet.Producer(router);
+            client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            Console.ReadLine();
+        }
+
+        private static string Serialize(ObjetoTeste obj)
+        {
+            string vs = null;
+            vs = JsonConvert.SerializeObject(obj);
+            return vs;
+        }
+    }
+
+    class ObjetoTeste
+    {
+
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
