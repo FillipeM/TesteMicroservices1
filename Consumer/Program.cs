3c8fd3801eea1e5077b1f50fe2a8446f9201ef4e
FillipeM <fillipemartins.alves@gmail.com>
2020-03-09 11:57:54 -0300

    adicionado dll confluent.kafka

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,9 +1,12 @@
-﻿using KafkaNet;
+﻿using Confluent.Kafka;
+using KafkaNet;
 using KafkaNet.Model;
+using KafkaNet.Protocol;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Consumer
 {
@@ -11,24 +14,92 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            string topic = "IDGTestTopic";
-            Uri uri = new Uri("http://localhost:9093");
-            var options = new KafkaOptions(uri);
-            var broker = new BrokerRouter(options);
-            var cliente = new KafkaNet.Consumer(new ConsumerOptions(topic, broker));
+            #region KafkaNet
+            //string topic = "IDGTestTopic";
+            //Uri uri = new Uri("http://localhost:9092");
+            //var options = new KafkaOptions(uri);
+            //var broker = new BrokerRouter(options);
+            //OffsetPosition[] position = new OffsetPosition[] { };
+            //var cliente = new KafkaNet.Consumer(new ConsumerOptions(topic, broker));
 
-            List<ObjetoTeste> list = new List<ObjetoTeste>();
+            //List<ObjetoTeste> list = new List<ObjetoTeste>();
+            //foreach(var message in cliente.Consume())
+            //{
+            //    ObjetoTeste obj = new ObjetoTeste();
+            //    Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+            //    //var commit = new OffsetCommitRequest
+            //    //{
+            //    //    OffsetCommits = new List<OffsetCommit>
+            //    //    {
+            //    //        new OffsetCommit
+            //    //        {
+            //    //            PartitionId = message.Meta.PartitionId,
+            //    //            Topic = topic,
+            //    //            Offset = message.Meta.Offset,
+            //    //            Metadata = message.Meta.ToString()
+            //    //        }
+            //    //    }
+            //    //};
+            //    //var r = broker.SelectBrokerRoute(topic, message.Meta.PartitionId);
+            //    //r.Connection.SendAsync(commit);
+            //    obj = JsonConvert.DeserializeObject<ObjetoTeste>(Encoding.UTF8.GetString(message.Value));
+            //    list.Add(obj);
+            //}
 
-            foreach(var message in cliente.Consume())
+            //Console.WriteLine(list.Count.ToString());
+            //Console.ReadLine();
+            #endregion
+
+            #region Confluent
+            var config = new ConsumerConfig
             {
-                ObjetoTeste obj = new ObjetoTeste();
-                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
-                obj = JsonConvert.DeserializeObject<ObjetoTeste>(Encoding.UTF8.GetString(message.Value));
-                list.Add(obj);
-            }
+                BootstrapServers = "localhost:9092",
+                GroupId = "testee",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
 
-            Console.WriteLine(list.Count.ToString());
-            Console.ReadLine();
+            const int commitPeriod = 5;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("testeConfluent");
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cts.Cancel();
+                };
+
+                while (true)
+                {
+                    var consumeResult = consumer.Consume(cts.Token);
+                    if (consumeResult.IsPartitionEOF)
+                    {
+                        Console.WriteLine($"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}");
+                        continue;
+                    }
+                    Console.WriteLine($"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Value}");
+
+                    if (consumeResult.Offset % commitPeriod == 0)
+                    {
+                        // The Commit method sends a "commit offsets" request to the Kafka
+                        // cluster and synchronously waits for the response. This is very
+                        // slow compared to the rate at which the consumer is capable of
+                        // consuming messages. A high performance application will typically
+                        // commit offsets relatively infrequently and be designed handle
+                        // duplicate messages in the event of failure.
+                        try
+                        {
+                            consumer.Commit(consumeResult);
+                        }
+                        catch (KafkaException e)
+                        {
+                            Console.WriteLine($"Commit error: {e.Error.Reason}");
+                        }
+                    }
+                }
+            }
+            #endregion
+
         }
     }
 
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,9 +1,11 @@
-﻿using KafkaNet;
+﻿using Confluent.Kafka;
+using KafkaNet;
 using KafkaNet.Model;
 using KafkaNet.Protocol;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace Producer
@@ -12,32 +14,57 @@ namespace Producer
     {
         static void Main(string[] args)
         {
-            string payLoad = "Welcome to Kafka";
-            ObjetoTeste obj = new ObjetoTeste();
-            obj.Codigo = 1;
-            obj.Descricao = "Teste";
-            string topic = "IDGTestTopic";
-            Message msg = new Message(Serialize(obj));
-            Uri uri = new Uri("http://localhost:9093");
-            var options = new KafkaOptions(uri);
-            var router = new BrokerRouter(options);
-            var client = new KafkaNet.Producer(router);
-            client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            #region KafkaNet
+            //string payLoad = "Welcome to Kafka";
+            //ObjetoTeste obj = new ObjetoTeste();
+            //obj.Codigo = 1;
+            //obj.Descricao = "Teste";
+            //string topic = "IDGTestTopic";
+            //Message msg = new Message(Serialize(obj));
+            //Uri uri = new Uri("http://localhost:9093");
+            //var options = new KafkaOptions(uri);
+            //var router = new BrokerRouter(options);
+            //var client = new KafkaNet.Producer(router);
+            //client.SendMessageAsync(topic, new List<Message> { msg }).Wait();
+            //Console.ReadLine();
+            #endregion
+
+            #region Confluent
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var t = producer.ProduceAsync("testeConfluent", new Message<Null, string> { Value = "teste2"});
+                t.ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        throw new Exception("Errrrrrrrrrrrro");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Wrote to offset: {task.Result.Offset}");
+                        Console.ReadLine();
+                    }
+                });
+            }
             Console.ReadLine();
+
+            #endregion
         }
 
-        private static string Serialize(ObjetoTeste obj)
-        {
-            string vs = null;
-            vs = JsonConvert.SerializeObject(obj);
-            return vs;
-        }
-    }
-
-    class ObjetoTeste
+    private static string Serialize(ObjetoTeste obj)
     {
-
-        public int Codigo { get; set; }
-        public string Descricao { get; set; }
+        string vs = null;
+        vs = JsonConvert.SerializeObject(obj);
+        return vs;
     }
+}
+
+class ObjetoTeste
+{
+
+    public int Codigo { get; set; }
+    public string Descricao { get; set; }
+}
 }
